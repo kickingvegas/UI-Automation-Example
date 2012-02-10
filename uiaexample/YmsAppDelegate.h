b7706e5199c5417a3b44290c67018740c5588b2f
Charles Choi <charles.choi@yummymelon.com>
2012-02-09 21:56:01 -0800

    Initial Commit

---
+++ uiaexample/YmsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YmsAppDelegate.h
+//  uiaexample
+//
+//  Created by Charles Y. Choi on 2/9/12.
+//  Copyright (c) 2012 Yummy Melon Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YmsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ uiaexample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uiaexample
+//
+//  Created by Charles Y. Choi on 2/9/12.
+//  Copyright (c) 2012 Yummy Melon Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YmsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YmsAppDelegate class]));
+    }
+}
